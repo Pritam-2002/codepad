c779be25e1bea5187fdbc1c6c86576f469154673
Pritam <mukherjeep7654321@gmail.com>
2023-07-23 16:35:42 +0530

    nothing

--- App.jsx
+++ App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import './App.css'
 import A from './components/a'
 
@@ -9,27 +9,16 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <Router>
-    <div>
-      {/* Place your common components, headers, etc. here */}
-      <Switch>
-        
-        <Route exact path="/A">
-          
+    
    
-      <A />
+      <A></A>
 
-      </Route>
-          
-        </Switch>
-      </div>
-    </Router>
-      
+     
        
       
      
     
-  );
+  )
 }
 
 export default App
